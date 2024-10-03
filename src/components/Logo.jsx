22d8463f2628bd8c638ac46f952531d2f56fe227
Gareth Dean <gazdean@hotmail.com>
2024-10-03 10:17:07 +0100

    default avatar added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import logo from '../assets/images/logo.png'
 
-export default function ({height='h-14', responsiveClasses}) {
+export default function ({height='h-14', logoClasses}) {
   return (
     <img
         alt="Your Company"
         src={logo}
-        className={`w-auto rounded-md ${height} ${responsiveClasses}`}
+        className={`w-auto rounded-md ${height} ${logoClasses}`}
     />
   )
 }
--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router'
 import { NavLink } from 'react-router-dom'
 
 export default function NavLinks({classes, setIsOpen}) {
+    const [currentPathname, setCurrentPathname] = useState('')
 
+    const location = useLocation()
+
+    useEffect(()=> {
+        setCurrentPathname(location.pathname)
+    }, [location])
+
+   
   return (
     <>
         <NavLink
             to={'/sign-in'}
             id='navbar-signin'
-            className={classes}
+            className={`${classes} ${currentPathname === '/sign-in' ? 'bg-second border-blue-500' : 'border-[rgba(59,130,246,0.0)]'}`}
             onClick={()=>setIsOpen(false)}
         >
             Sign in
@@ -16,7 +27,7 @@ export default function NavLinks({classes, setIsOpen}) {
         <NavLink
             to={'/register'}
             id='navbar-register'
-            className={classes}
+            className={`${classes} ${currentPathname === '/register' ? 'bg-second border-blue-500' : 'border-[rgba(59,130,246,0.0)]'}`}
             onClick={()=>setIsOpen(false)}
         >
             Register
@@ -24,7 +35,7 @@ export default function NavLinks({classes, setIsOpen}) {
         <NavLink
             to={'/profile'}
             id='navbar-profile'
-            className={classes}
+            className={`${classes} ${currentPathname === '/profile' ? 'bg-second border-blue-500' : 'border-[rgba(59,130,246,0.0)]'}`}
             onClick={()=>setIsOpen(false)}
         >
             profile
